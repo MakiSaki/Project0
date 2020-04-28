2ceb717f6ce718de7c8a01c503d1f6946a2ceba1
Nicole Evans <nicoleevans@Keiths-MBP.attlocal.net>
2020-04-27 22:47:04 -0400

    remove unused code

--- secpage/resources.py
+++
@@ -1,6 +0,0 @@
-from tastypie.resources import ModelResource
-from secpage.models import Note
-class NoteResource(ModelResource):
-    class Meta:
-        queryset = Note.objects.all()
-        resource_name = 'note'
